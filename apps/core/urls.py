d4fa22d92d43f082a6727628178e78a5aff637e5
Herson1809 <hersonstan18@gmail.com>
2025-09-19 19:59:55 -0600

    Update urls.py

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -2,5 +2,6 @@ from django.urls import re_path
 from . import views
 
 urlpatterns = [
+    # otras rutas que ya tengas…
     re_path(r"^reports/income/?$", views.income_report, name="income-report"),
 ]
