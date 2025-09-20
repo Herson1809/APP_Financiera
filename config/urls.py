2712772da84bff3dd32ecbc784472323a451a451
Herson1809 <hersonstan18@gmail.com>
2025-09-19 20:04:04 -0600

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,8 @@
+# config/urls.py
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("apps.core.urls")),
+    path("", include("apps.core.urls")),  # ← aquí se cargan home, foundation, reports, etc.
 ]
