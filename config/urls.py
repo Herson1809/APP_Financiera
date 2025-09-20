8870d14801c115a6c01db498c5830b9eb5aaf169
Herson1809 <hersonstan18@gmail.com>
2025-09-19 20:00:14 -0600

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("apps.core.urls")),  # << usa las rutas de core
+    path("", include("apps.core.urls")),
 ]
