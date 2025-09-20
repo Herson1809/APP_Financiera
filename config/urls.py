c4cf4eb5b162ced1ecb96bffec6fa309e5c2e15a
Herson1809 <hersonstan18@gmail.com>
2025-09-19 19:54:02 -0600

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,14 +1,7 @@
 from django.contrib import admin
-from django.urls import path
-from apps.core import views as core_views
+from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-
-    # Conectar tus plantillas existentes (SIN crear archivos nuevos)
-    path('', core_views.home, name='home'),                 # templates/core/home.html
-    path('foundation/', core_views.foundation, name='foundation'),  # templates/core/dashboard.html
-
-    # EERR (ya existente)
-    path('reports/income', core_views.income_report, name='income-report'),
+    path("admin/", admin.site.urls),
+    path("", include("apps.core.urls")),  # << usa las rutas de core
 ]
